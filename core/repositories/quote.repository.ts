6163203b7774322bb24721cae4c50d4affa403d9
laJenny4 <jenniferPaco412@gmail.com>
2025-08-15 17:47:33 -0400

    Added logic to consume Binance API and calculate the quote

--- core/repositories/quote.repository.ts
+++ core/repositories/quote.repository.ts
@@ -1,5 +1,6 @@
 import {createClient} from "@supabase/supabase-js";
 import {GetQuoteDto} from '../dto/quote.dto';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export class QuoteRepository {
     private supabase;
@@ -12,7 +13,33 @@ export class QuoteRepository {
     }
 
     async getQuote(getQuoteDto: GetQuoteDto): Promise<number> {
+        const payload = {
+            page: 1,
+            rows: 20,
+            payTypes: [],
+            asset: getQuoteDto.cryptoCurrency,
+            fiat: getQuoteDto.fiatCurrency,
+            tradeType: "SELL",
+            network: getQuoteDto.network
+        };
 
+        const response = await fetch("https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload),
+        });
+
+        const result = await response.json();
+
+        if (!result.data || result.data.length === 0) {
+            throw new Error("No quotes available");
+        }
+
+        const rates = result.data.map((item: any) => parseFloat(item.adv.price));
+        const bestRate = Math.min(...rates);
+
+        return bestRate;
     }
+
 
 }
--- core/services/qoute.service.ts
+++ core/services/qoute.service.ts
@@ -8,14 +8,32 @@ export class QuoteService {
     constructor() {
         this.quoteRepository = new QuoteRepository();
     }
-    async getQuote(getQuoteDto: GetQuoteDto){
-        const quote = await this.quoteRepository.getQuote(getQuoteDto);
 
-        if (!quote) {
+    async getQuote(getQuoteDto: GetQuoteDto) {
+        if (getQuoteDto.fiatAmount <= 0) {
+            throw new Error('Fiat amount must be greater than 0');
+        }
+        if (getQuoteDto.fiatAmount < 10) {
+            throw new Error('Minimum fiat amount is 10');
+        }
+        if (getQuoteDto.fiatAmount >10000) {
+            throw new Error('Maximum fiat amount is 10000');
+        }
+
+        const bestRate = await this.quoteRepository.getQuote(getQuoteDto);
+
+        if (!bestRate) {
             throw new Error('Failed to get quote');
         }
-        return quote;
-    }
 
+        const usdtAmount = parseFloat((getQuoteDto.fiatAmount / bestRate).toFixed(6));
+
+        return {
+            fiatAmount: getQuoteDto.fiatAmount,
+            fiatCurrency: getQuoteDto.fiatCurrency,
+            usdtAmount,
+            rate: bestRate,
+        };
+    }
 
 }
