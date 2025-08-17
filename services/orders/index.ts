fee9cf72c0a46f462629f51bcf6492ff949a03dd
laJenny4 <jenniferPaco412@gmail.com>
2025-08-17 13:29:49 -0400

    test index

--- services/orders/index.ts
+++ services/orders/index.ts
@@ -1,10 +1,10 @@
 // Punto de entrada - decide qué servicio usar
 // import { API_CONFIG } from '../../config/api';
 import { ordersMockService } from './orders.mock';
-// import { ordersApiService } from './orders.api';
-
+//import { ordersApiService } from './orders.api';
+//AQUI SE HACE EL FETCH
 // Exporta el servicio según configuración
-export const ordersService = ordersMockService
+export const ordersService = ordersMockService;
 // TODO: Uncomment the next line to use the real API service
 //  API_CONFIG.useMockServices 
 //   ? ordersMockService 
