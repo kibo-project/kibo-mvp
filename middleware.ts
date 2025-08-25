eaed2d5bc43b428925498cb0de0eaef0e0f53062
laJenny4 <jenniferPaco412@gmail.com>
2025-08-25 12:18:33 -0400

    Sync dev branch with remote dev, tested and working

--- middleware.ts
+++ middleware.ts
@@ -36,6 +36,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     "/api/auth/profile",
-    "/api/orders/:path*",
+  //  "/api/orders/:path*",
   ],
 };
