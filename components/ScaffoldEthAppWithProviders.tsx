5a696f46b51f461c355a5a27a4114eb5f1717972
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2025-09-03 20:12:52 -0400

    refactor: clean up unused imports and comments in ScaffoldEthAppWithProviders component

--- components/ScaffoldEthAppWithProviders.tsx
+++ components/ScaffoldEthAppWithProviders.tsx
@@ -1,22 +1,14 @@
 "use client";
 
-// import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { NavigationApp } from "./NavigationApp";
-// import { PrivyProvider } from "@privy-io/react-auth";
-// import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
-// import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
-// import { WagmiProvider } from "wagmi";
-import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
-  // useInitializeNativeCurrencyPrice();
   const pathname = usePathname();
-  // Lista de rutas que NO deben usar el layout principal
   const excludedRoutes = ["/login", "/order/camera", "/order/confirmation-payment"];
 
   const shouldExclude = excludedRoutes.includes(pathname);
