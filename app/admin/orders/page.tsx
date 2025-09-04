2e354b762ac0a042b9e96a51f55561a62ea9ac89
laJenny4 <jenniferPaco412@gmail.com>
2025-09-04 16:38:41 -0400

    new page admin/page.tsx using middleware, add component statCard

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { NextPage } from "next";
+
+const Orders: NextPage = () => {
+  return (
+    <div>
+      <h1>Orders Page</h1>
+    </div>
+  );
+};
+
+export default Orders;
